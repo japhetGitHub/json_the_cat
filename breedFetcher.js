4be6dffd72f6561efe827e3cd0cd1e0b51e3abe0
Japhet Elias <japhet9@gmail.com>
2021-09-27 01:02:07 +0000

    handled edge case: request failed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,10 @@
 const request = require('request');
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
+  if (error) {
+    console.error("Request Error:", error)
+    return;
+  }
   const data = JSON.parse(body);
 
   if(data.length === 0) {
