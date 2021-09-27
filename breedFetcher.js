7373b270ae64321f330e0201f1d415ea42e6a46f
Japhet Elias <japhet9@gmail.com>
2021-09-27 01:02:53 +0000

    linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,12 +2,12 @@ const request = require('request');
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
   if (error) {
-    console.error("Request Error:", error)
+    console.error("Request Error:", error);
     return;
   }
   const data = JSON.parse(body);
 
-  if(data.length === 0) {
+  if (data.length === 0) {
     console.log("Breed not found.");
   } else {
     console.log(data[0].description);
