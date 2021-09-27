9f78cbe42308794fa6d442ab44e459eace7a553d
Japhet Elias <japhet9@gmail.com>
2021-09-27 00:52:22 +0000

    breed chosen via user input

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=siberian`, (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
   const data = JSON.parse(body);
   console.log(data[0].description);
   console.log(typeof data);
